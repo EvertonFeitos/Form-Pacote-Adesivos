45cd65ca4ff0b408a2f8721d294338459726c9b7
Everton Feitosa <99797654+EvertonFeitos@users.noreply.github.com>
2022-03-21 10:01:07 -0300

    Add files via upload
    inicio modificação js

---
+++ script.js
@@ -0,0 +1,20 @@
+function QuantSubtrair() {
+
+	if (document.getElementById('quantidade').value == 0) {
+
+		alert('Não é possivel solicitar um valor menor que zero!!!')
+	}
+
+		else
+		{
+			document.getElementById('quantidade').value--
+		}
+
+}
+
+function QuantSoma() {
+
+	document.getElementById('quantidade').value++
+
+	}
+

efcdbb76c52bd939ebdcb088ee30819aa3357d23
Everton Feitosa <99797654+EvertonFeitos@users.noreply.github.com>
2022-03-22 23:09:06 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,67 +1,45 @@
 /*botoes quantidade*/
-
 function QuantSubtrair() {
-
 	if (document.getElementById('quantidade').value == 0) {
-
-		alert('Não é possivel solicitar um valor menor que zero!!!')
+	alert('Não é possivel solicitar um valor menor que zero!!!')
+	}
+	else
+	{
+	document.getElementById('quantidade').value--
+	}
 	}
 
-		else
-		{
-			document.getElementById('quantidade').value--
-		}
-
-}
 
 function QuantSoma() {
-
 	document.getElementById('quantidade').value++
-
 	}
 
-
-	function validarCheckbox(){
-
-
+function validarCheckbox(){
 	var adesivos = document.getElementsByName('Adesivo');
 	var arrayAdesivosSelecionados = []
-
-    for (var i = 0; i < adesivos.length; i++)
+	for (var i = 0; i < adesivos.length; i++)
 	{
-        if ( adesivos[i].checked ) {
-            if(adesivos[i].value == "React"){
-               arrayAdesivosSelecionados.push(" React");
-				}else if (adesivos[i].value == "Vue") {
-               arrayAdesivosSelecionados.push(" Vue");
-
-            }else if (adesivos[i].value == "Angular") {
-               arrayAdesivosSelecionados.push(" Angular");
-
-            } else if (adesivos[i].value == "JavaScript") {
-        		arrayAdesivosSelecionados.push(" JavaScript");
-        }
-
-		adesivosSelecionados = arrayAdesivosSelecionados
-		arrayAdesivosSelecionados = adesivosSelecionados
-
-
-
-    }
-
-
-
-}
-
-}
+	if ( adesivos[i].checked ) {
+	if(adesivos[i].value == "React"){
+	arrayAdesivosSelecionados.push(" React");
+	}else if (adesivos[i].value == "Vue") {
+	arrayAdesivosSelecionados.push(" Vue");
+	}else if (adesivos[i].value == "Angular") {
+	arrayAdesivosSelecionados.push(" Angular");
+	} else if (adesivos[i].value == "JavaScript") {
+	arrayAdesivosSelecionados.push(" JavaScript");
+	}
+	var adesivosSelecionados = arrayAdesivosSelecionados
+	arrayAdesivosSelecionados = adesivosSelecionados
+	}
+	}
+	return arrayAdesivosSelecionados
+	}
 
 function enviar()
-{
+	{
 	var inputQuantidade = document.getElementById('quantidade').value
-		var campoObservacao = document.getElementById('observacoes').value
-		var adesivosEscolhidos = validarCheckbox()
-
-		window.alert("Adesivos escolhidos:\n" + adesivosEscolhidos + "\n\n\Quantidade:\n" + inputQuantidade +" unidades de cada.\n\nObservações:\n\n" + campoObservacao)
-		}
-
-
+	var campoObservacao = document.getElementById('observacoes').value
+	var adesivosEscolhidos = validarCheckbox()
+	window.alert("Adesivos escolhidos:\n\n" + adesivosEscolhidos + "\n\n\Quantidade:\n\n" + inputQuantidade +" unidades de cada.\n\nObservações:\n\n" + campoObservacao)
+	}
